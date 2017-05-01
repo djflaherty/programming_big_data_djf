a66d78135be9526e3f3b9bd9e906e66ad1667ba3
djflaherty <10349680@mydbs.ie>
2017-05-01 11:17:16 +0100

    added code to create bar chart for active hours

--- CA04/vm_analysis.R
+++ CA04/vm_analysis.R
@@ -2,6 +2,10 @@
 git.dir <- 'C:/Users/deird/programming_big_data_djf/CA04'
 setwd(git.dir)
 
+####################################################################################
+#                           Active Days
+####################################################################################
+
 # load in the active days CSV file and set col headings
 mydata <- read.csv('active_days.csv', header = TRUE, col.names = c("Day","NbrCommits"))
 fix(mydata)
@@ -22,3 +26,30 @@ barplot(commits,names.arg = days,xlab = "Weekday",ylab = "Number of Commits",col
 
 # Save the file
 dev.off()
+
+####################################################################################
+#                           Active Hours
+####################################################################################
+
+# load in the active days CSV file and set col headings
+mydata <- read.csv('active_hours.csv', header = TRUE, col.names = c("Hour","NbrCommits"))
+fix(mydata)
+
+#convert the number of commits to numeric
+mydata$NbrCommits <- as.numeric(mydata$NbrCommits)
+
+#create vectors for the days and the number of commits
+hours <- mydata$Hour
+commits <- mydata$NbrCommits
+
+# Give the chart file a name
+png(file = "barchart_active_hours.png")
+
+# Plot the bar chart
+barplot(commits,names.arg = hours,xlab = "Hour",ylab = "Number of Commits",col = "blue",
+        main = "Active Hours",border = "red")
+
+# Save the file
+dev.off()
+
+####################################################################################
