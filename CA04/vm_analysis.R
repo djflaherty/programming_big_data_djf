119c0479afb195a481ce51ae417dcb855639966f
djflaherty <10349680@mydbs.ie>
2017-05-01 13:09:18 +0100

    added code to create stacked bar chart for change totals per author

--- CA04/vm_analysis.R
+++ CA04/vm_analysis.R
@@ -10,46 +10,80 @@ setwd(git.dir)
 mydata <- read.csv('active_days.csv', header = TRUE, col.names = c("Day","NbrCommits"))
 fix(mydata)
 
-#convert the number of commits to numeric
-mydata$NbrCommits <- as.numeric(mydata$NbrCommits)
-
 #create vectors for the days and the number of commits
 days <- mydata$Day
 commits <- mydata$NbrCommits
 
-# Give the chart file a name
-png(file = "barchart_active_days.png")
+# give the bar chart file a name
+png(file = 'barchart_active_days.png')
 
-# Plot the bar chart
+# plot the bar chart
 barplot(commits,names.arg = days,xlab = "Weekday",ylab = "Number of Commits",col = "blue",
         main = "Active Days",border = "red")
 
-# Save the file
+# save the file
 dev.off()
 
 ####################################################################################
 #                           Active Hours
 ####################################################################################
 
-# load in the active days CSV file and set col headings
+# load in the active hours CSV file and set col headings
 mydata <- read.csv('active_hours.csv', header = TRUE, col.names = c("Hour","NbrCommits"))
 fix(mydata)
 
-#convert the number of commits to numeric
-mydata$NbrCommits <- as.numeric(mydata$NbrCommits)
-
-#create vectors for the days and the number of commits
+# create vectors for the hours and the number of commits
 hours <- mydata$Hour
 commits <- mydata$NbrCommits
 
-# Give the chart file a name
-png(file = "barchart_active_hours.png")
+# give the bar chart file a name
+png(file = 'barchart_active_hours.png')
 
-# Plot the bar chart
+# plot the bar chart
 barplot(commits,names.arg = hours,xlab = "Hour",ylab = "Number of Commits",col = "blue",
         main = "Active Hours",border = "red")
 
-# Save the file
+# save the file
 dev.off()
 
 ####################################################################################
+#                           Change Totals per Author
+####################################################################################
+
+# load in the change totals CSV file and set col headings
+mydata <- read.csv('change_totals.csv', header = TRUE)
+fix(mydata)
+
+# re-order the columns and drop the first col
+mydata <- mydata[c("author", "additions", "deletions","modifications","replacements")]
+fix(mydata)
+
+# replace the system generated author name
+mydata$author <- gsub('/OU=Domain Control Validated/CN=svn.company.net','System', mydata$author)
+fix(mydata)
+
+# create list of authors for the bar chart
+authors <- mydata$author
+
+# create legend for the bar chart
+totals <- colnames(mydata)
+totals <- totals[totals != 'author']
+
+# create colours for the bar chart
+colours <- c('green', 'red', 'blue','orange')
+
+# create a matrix for the change totals & then transpose it
+vals <- data.matrix(mydata[2:5])
+newvals <- t(vals)
+
+# give the stacked bar chart file a name
+png(file = "barchart_change_totals.png")
+
+# plot the stacked bar chart with labels perpendicular to x-axis
+barplot(newvals,main="Change Totals by Author",names.arg = authors,xlab = "Author",ylab = "Number of Changes",las=2,col = colours)
+
+# add the legend to the chart
+legend("topright", totals, cex = 1.1, fill = colours)
+
+# save the file
+dev.off()
