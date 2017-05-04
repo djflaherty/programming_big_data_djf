4182ff15e855bf6c885ee84f806570423f3ce60c
djflaherty <10349680@mydbs.ie>
2017-05-04 20:22:29 +0100

    added 4 more calculator functions

--- CA05/calculator.R
+++ CA05/calculator.R
@@ -12,3 +12,27 @@ subtract_two_numbers <- function(num1, num2){
 multiply_two_numbers <- function(num1, num2){ 
   num1 * num2 
 }
+
+#Function4: divide one number by another
+divide_two_numbers <- function(num1, num2){ 
+  if (num2 == 0) {
+    'undefined' 
+  } else {
+    num1 / num2
+  }
+}
+
+#Function5: exponentiate one number by another
+exponentiate_two_numbers <- function(num1, num2){ 
+  num1 ** num2 
+}
+
+#Function6: square a number
+square <- function(num){ 
+  num ** 2
+}
+
+#Function7: cube a number
+cube <- function(num){ 
+  num ** 3
+}
