3e8e4d29729d0941d7e84b5ad9cb6983337533a3
djflaherty <10349680@mydbs.ie>
2017-05-04 20:13:02 +0100

    calculator in R with 3 functions

---
+++ CA05/calculator.R
@@ -0,0 +1,14 @@
+#Function1: add two numbers together
+add_two_numbers <- function(num1, num2){ 
+  num1 + num2 
+}
+
+#Function2: subtract one number from another
+subtract_two_numbers <- function(num1, num2){ 
+  num1 - num2 
+}
+
+#Function3: multiply two numbers
+multiply_two_numbers <- function(num1, num2){ 
+  num1 * num2 
+}
