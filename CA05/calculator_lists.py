2d91a00f6a02c06bd860e50dd2d1ec637f1546e8
djflaherty <10349680@mydbs.ie>
2017-05-12 19:06:27 +0100

    adding fourth function

--- CA05/calculator_lists.py
+++ CA05/calculator_lists.py
@@ -17,6 +17,18 @@ def subtract_lists(list1, list2):
 def divide_lists(list1,list2):
     return map(lambda x, y: x/float(y) if y!= 0 else 'nan', list1, list2)
 
+# Function4a: original calculator function to multiply two numbers
+def multiply(first, second):
+    number_types = (int, long, float, complex)
+    if isinstance(first, number_types) and isinstance(second, number_types):
+        return first * second
+    else:
+         return 'error'
+         
+# Function4b: Adapt the above function to work with lists using Map
+# each element of list1 will be multiplied by the corresponding element of list2
+def multiply_list(list1, list2): 
+    return map(multiply, list1, list2)
 
 ##########################################
 #       Calls
@@ -25,3 +37,7 @@ def divide_lists(list1,list2):
 print add_lists([2,5,4],[2,3,0])
 print subtract_lists([2,5,4,3],[2,3,0,4])
 print divide_lists([1,2,3],[2,1,0])
+
+print multiply_list([2,5,4,3],[2,3,0,4])
+print multiply_list([2,5,4,3],['mary',3,0,4])
+
